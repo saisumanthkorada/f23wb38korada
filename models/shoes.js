baa354a753825d1311e667be3b5f6fc0e9a6fd0d
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2023-11-09 12:19:40 -0600

    changes made due to wrong file

--- models/shoes.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const shoesSchema = mongoose.Schema({
-Brand: String,
-Ambassador: String,
-price: String
-})
-module.exports = mongoose.model("shoe", shoesSchema)
-
